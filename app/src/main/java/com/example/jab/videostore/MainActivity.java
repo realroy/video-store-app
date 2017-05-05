06398140877918e550392c51f6513978eed5cf6c
realroy <realroyrealboy@gmail.com>
2017-05-06 00:02:36 +0700

    Done apply firebase database.

--- app/src/main/java/com/example/jab/videostore/MainActivity.java
+++ app/src/main/java/com/example/jab/videostore/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.jab.videostore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        db.getReference().child("test").setValue("Done!");
+
     }
 }
