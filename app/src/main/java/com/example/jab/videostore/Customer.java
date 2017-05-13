a4cfccc722116683ef7655d4438676aa7e38a43c
Kan <kan86197@gmail.com>
2017-05-13 14:09:21 +0700

    Customer added

---
+++ app/src/main/java/com/example/jab/videostore/Customer.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+	private String id, password;
+	private double balance;
+	private List<Video> cart;
+	public Customer(String id, String password, int balance) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.password = password;
+		this.balance = balance;
+		cart = new ArrayList<Video>();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public void addToCart(Video v){
+		cart.add(v);
+	}
+	public void checkOut(){
+		double totalCost = 0;
+		for(Video vid : cart){
+			totalCost += vid.getPrice();
+		}
+		balance -= totalCost;
+	}
+	
+}
