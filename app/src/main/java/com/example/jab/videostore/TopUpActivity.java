c12af80b710609e6ecd7a71a5cbc24f25cb17d5e
realroy <roiboon.c@ku.th>
2017-05-25 16:15:53 +0700

    Replace mainStore with StoreActivity

--- app/src/main/java/com/example/jab/videostore/TopUpActivity.java
+++ app/src/main/java/com/example/jab/videostore/TopUpActivity.java
@@ -1,9 +1,9 @@
 package com.example.jab.videostore;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class TopUp extends AppCompatActivity {
+public class TopUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
