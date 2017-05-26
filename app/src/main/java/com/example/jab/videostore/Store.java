8a86817196a93e98a6b5269523b4c7eef34d6d98
Kan <kan86197@gmail.com>
2017-05-26 15:38:30 +0700

    Notify when restock

--- app/src/main/java/com/example/jab/videostore/Store.java
+++ app/src/main/java/com/example/jab/videostore/Store.java
@@ -2,25 +2,26 @@ package com.example.jab.videostore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
 
 /**
  * Created by Jab on 5/13/2017 AD.
  */
- 
-public class Store {
+
+public class Store extends Observable {
 	private static Store instance = null;
 	private List<Video> videos;
 	private Store(){
 		// wait for code to retreive from database
 		videos = new ArrayList<>();
-		Video v1 = new Video("Jeeper Creeper",19,"Horor","Creep guy try to eat gay.");
-		Video v2 = new Video("Brother of Grimby",50,"Comedy","Shitty duo brother wreck the world.");
+		Video v1 = new Video("Jeeper Creeper",19,"Horor","Creep guy try to eat gay.", 0);
+		Video v2 = new Video("Brother of Grimby",50,"Comedy","Shitty duo brother wreck the world.", 1);
 		videos.add(v1);
 		videos.add(v2);
 	}
-	public Store getInstance(){
+	public static Store getInstance(){
 		if(instance == null){
-			 instance = new Store();
+			instance = new Store();
 		}
 		return instance;
 	}
@@ -29,5 +30,32 @@ public class Store {
 	}
 	public Video getVideo(int index){
 		return videos.get(index);
+	}
+
+	public void restock(Video vid){
+		if(videos.get(videos.indexOf(vid)).productAvailable()) 
+			System.out.println("Product still available.");
+		else {
+			videos.get(videos.indexOf(vid)).restock();
+			setChanged();
+			notifyObservers(vid);
+		}
+	}
+
+	public void restockAll(){
+		for(Video vid: videos){
+			if(!vid.productAvailable()) vid.restock();
+		}
+		setChanged();
+		notifyObservers(videos);
+	}
+
+	public void buy(Video vid){
+		if(vid.productAvailable()){
+			Video temp = videos.get(videos.indexOf(vid));
+			videos.get(videos.indexOf(vid)).setAmount(temp.getAmount() - 1);
+		}	else{
+			System.out.println(vid.getName() + " is out of stock");
+		}
 	}
 }
